e6cf8f324adf19a2227dde71b7b0a227f7f7feac
stangah <pstanger@gmail.com>
2014-01-10 14:34:10 -0800

    Integrated fred's solutions

--- src/solvers.js
+++ src/solvers.js
@@ -1,5 +1,5 @@
-/*           _                    
-   ___  ___ | |_   _____ _ __ ___ 
+/*           _
+   ___  ___ | |_   _____ _ __ ___
   / __|/ _ \| \ \ / / _ \ '__/ __|
   \__ \ (_) | |\ V /  __/ |  \__ \
   |___/\___/|_| \_/ \___|_|  |___/
@@ -12,19 +12,36 @@
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
+/* START SOLUTION */
+var findRookSolution = function(board, startRow, rows, callback){
+  if( startRow === rows ){
+    return callback(board);
+  }
+
+  for( var i = 0; i < rows; i++ ){
+    board.togglePiece(startRow, i);
+    if( !board.hasAnyRooksConflicts() ){
+      var result = findRookSolution(board, startRow+1, rows, callback);
+      if( result ){ return result; }
+    }
+    board.togglePiece(startRow, i);
+  }
+};
+
+/* END SOLUTION */
 window.findNRooksSolution = function(n){
   /* START_PROMPT
     var solution = undefined; //fixme
   END_PROMPT */
-
   /* START_SOLUTION */
-    var solution = _.range(n).map(function(rowIndex){
-      return _.range(n).map(function(colIndex){
-        return +(rowIndex === colIndex);
-      });
-    });
+  var solution;
+  var board = new Board({n:n});
+
+  findRookSolution(board, 0, n, function(board){
+    return solution = board.rows();
+  });
   /* END_SOLUTION */
-  
+
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
@@ -38,10 +55,12 @@ window.countNRooksSolutions = function(n){
   END_PROMPT */
 
   /* START_SOLUTION */
-    function factorial (n){
-      return factorial[n] = n <= 1 ? 1 : factorial[n] || n * factorial(n-1);
-    };
-    var solutionCount = factorial(n); //fixme
+  var solutionCount = 0;
+  var board = new Board({n:n});
+
+  findRookSolution(board, 0, n, function(board){
+    solutionCount++;
+  });
   /* END_SOLUTION */
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
@@ -51,7 +70,7 @@ window.countNRooksSolutions = function(n){
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
-window.findNQueensSolution = function(n){  
+window.findNQueensSolution = function(n){
   /* START_PROMPT
     var solution = undefined; //fixme
   END_PROMPT */
