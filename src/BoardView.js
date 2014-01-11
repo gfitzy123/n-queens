5420a9539e6665a630ca20b44e0ae84eabccd5b0
stangah <pstanger@gmail.com>
2014-01-11 10:49:25 -0800

    Style fixes

--- src/BoardView.js
+++ src/BoardView.js
@@ -1,7 +1,7 @@
 // This file is a Backbone View.
 // It's part of the board visualizer
 
-(function(){
+(function() {
 
   window.BoardView = Backbone.View.extend({
 
@@ -13,9 +13,10 @@
 
     render: function() {
       var model = this.model;
-      return this.$el.html(_(_.range(model.get('n'))).map(function(rowIndex){
-        return $('<tr class="row"/>').html(_(_.range(model.get('n'))).map(function(colIndex){
-          var $square = $('<td class="square"/>').on('click', function(e){
+
+      return this.$el.html(_(_.range(model.get('n'))).map(function(rowIndex) {
+        return $('<tr class="row"/>').html(_(_.range(model.get('n'))).map(function(colIndex) {
+          var $square = $('<td class="square"/>').on('click', function(e) {
             model.togglePiece(rowIndex, colIndex);
           }).addClass(['positive', 'negative'][(rowIndex + colIndex) % 2]);
           model.get(rowIndex)[colIndex] && $square.html('&#9813;');
