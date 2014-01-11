c70534357963326acde64398af257f86fae517b9
stangah <pstanger@gmail.com>
2014-01-11 10:47:45 -0800

    Style changes to match HR wiki

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -1,11 +1,12 @@
-describe("solvers", function() {
-  window.displayBoard = function(){};
+describe('solvers', function() {
+  window.displayBoard = function() {};
 
-  describe('findNRooksSolution()', function(){
+  describe('findNRooksSolution()', function() {
 
-    it('finds a valid solution for n of 1-8', function(){
-      _.range(1, 9).map(function(n){
+    it('finds a valid solution for n of 1-8', function() {
+      _.range(1, 9).map(function(n) {
         var solutionBoard = new Board(findNRooksSolution(n));
+
         expect(solutionBoard.get('n')).to.equal(n);
         expect(solutionBoard.hasAnyRooksConflicts()).to.be.equal(false);
       });
@@ -13,23 +14,25 @@ describe("solvers", function() {
 
   });
 
-  describe('countNRooksSolutions()', function(){
+  describe('countNRooksSolutions()', function() {
 
-    it('finds the number of valid solutions for n of 1-8', function(){
-      _.range(1, 9).map(function(n){
+    it('finds the number of valid solutions for n of 1-8', function() {
+      _.range(1, 9).map(function(n) {
         var solutionCount = countNRooksSolutions(n);
         var expectedSolutionCount = [1, 1, 2, 6, 24, 120, 720, 5040, 40320][n];
+
         expect(solutionCount).to.be.equal(expectedSolutionCount);
       });
     });
 
   });
 
-  describe('findNQueensSolution()', function(){
+  describe('findNQueensSolution()', function() {
 
-    it('finds a valid solution for n of 0-8', function(){
-      _.range(1, 8).map(function(n){
+    it('finds a valid solution for n of 0-8', function() {
+      _.range(1, 8).map(function(n) {
         var solutionBoard = new Board(findNQueensSolution(n));
+
         expect(solutionBoard.get('n')).to.equal(n);
         expect(solutionBoard.hasAnyQueensConflicts()).to.be.equal(false);
       });
@@ -37,12 +40,13 @@ describe("solvers", function() {
 
   });
 
-  describe('countNQueensSolutions()', function(){
+  describe('countNQueensSolutions()', function() {
 
-    it('finds the number of valid solutions for n of 0-8', function(){
-      _.range(0, 9).map(function(n){
+    it('finds the number of valid solutions for n of 0-8', function() {
+      _.range(0, 9).map(function(n) {
         var solutionCount = countNQueensSolutions(n);
         var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92][n];
+
         expect(solutionCount).to.be.equal(expectedSolutionCount);
       });
     });
