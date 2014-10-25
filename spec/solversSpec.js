f25783cfdc80989df7b41533efe61f9c9053aeac
Jonathan Reem <jonathan.reem@gmail.com>
2014-10-24 23:35:40 -0400

    (test) Special case n-queens of 2 and 3 because they have no solutions.

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -35,8 +35,9 @@ describe('solvers', function() {
 
   describe('findNQueensSolution()', function() {
 
-    it('finds a valid solution for n of 0-8', function() {
-      _.range(1, 8).map(function(n) {
+    it('finds a valid solution for n of 0-7', function() {
+      // Skip 2 and 3 because they have no solution.
+      [0, 1, 4, 5, 6, 7, 8].map(function(n) {
         var solutionBoard = new Board(findNQueensSolution(n));
         var numPieces = _.reduce(solutionBoard.rows(), function(memo, row) {
           return memo + _.reduce(row, function(memo, col) {
@@ -47,6 +48,19 @@ describe('solvers', function() {
         expect(solutionBoard.get('n')).to.equal(n);
         expect(numPieces).to.equal(n);
         expect(solutionBoard.hasAnyQueensConflicts()).to.be.equal(false);
+      });
+
+      // Check 2 and 3 for no solution
+      [2, 3].map(function (n) {
+        var solutionBoard = new Board(findNQueensSolution(n));
+        var numPieces = _.reduce(solutionBoard.rows(), function(memo, row) {
+          return memo + _.reduce(row, function(memo, col) {
+            return memo + col;
+          }, 0);
+        }, 0);
+
+        expect(numPieces).to.equal(0);
+        expect(solutionBoard.get('n')).to.equal(n);
       });
     });
 
